e76b213a746fd3218849a6d1bafa389a7be25e92
KiranBhatt <bhatt.dotnet@gmail.com>
2022-12-20 11:27:22 +0530

    Code refactoring

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -9,25 +10,15 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberListComponent implements OnInit {
 
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private membersService: MembersService) { }
 
   ngOnInit(): void {
 
-    this.loadMembers();
+    this.members$ = this.membersService.getMembers();
   }
 
-  loadMembers() {
-    this.membersService.getMembers().subscribe({
-      next: (members: Member[]) => {
-
-        this.members = members;
-      },
-      error: error => { console.log(error); }
-
-
-    });
-  }
+  
 
 }
