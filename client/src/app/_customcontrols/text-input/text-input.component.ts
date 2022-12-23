ef96a496e478cd096fc91fe4a4e891e9159d2be8
KiranBhatt <bhatt.dotnet@gmail.com>
2022-12-23 13:16:21 +0530

    Created a resuable text input control

---
+++ client/src/app/_customcontrols/text-input/text-input.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, Form, FormControl, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css']
+})
+export class TextInputComponent implements ControlValueAccessor {
+
+  @Input() label = '';
+  @Input() type = 'text';
+
+  constructor(@Self() public ngControl: NgControl) {
+
+    ngControl.valueAccessor = this;  // this refer to TextInputComponent
+
+  }
+
+  writeValue(obj: any): void {
+
+  }
+
+  registerOnChange(fn: any): void {
+
+  }
+
+  registerOnTouched(fn: any): void {
+
+  }
+
+  // Here control is property name.
+  // Suppose in ts file we have property like ---- country = new FormControl()
+  // so in HTML we will write  <input [formControl]="country">
+  get control(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
+
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { TextInputComponent } from './_customcontrols/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
